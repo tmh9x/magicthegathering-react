489c05da9ef20340ee3b7a2421d65ee6e7cdb95a
tmh9x <thomas.hermanu@gmail.com>
2022-07-19 17:57:59 +0200

    create card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NoImage from "../../assets/no-image.png";
+import "./Card.css";
+
+function Card(character) {
+  return (
+    <div className="card-container">
+      <div className="card-container-img">
+        {character.props.imageUrl ? (
+          <Link to={`${character.props.multiverseid}`}>
+            <img src={character.props.imageUrl} alt="" />
+          </Link>
+        ) : (
+          <div>
+            <img src={NoImage} alt="" />
+          </div>
+        )}
+      </div>
+      <div className="card-container-text">
+        <div>
+          <h2>{character.props.name}</h2>
+        </div>
+        <div>
+          <span className="bold">Type: </span>
+          {character.props.type}
+        </div>
+        <div>
+          <span className="bold">Toughness: </span>
+          {character.props.toughness}
+        </div>
+        <div>
+          <span className="bold">Mana Cost: </span>
+          {character.props.manaCost}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
