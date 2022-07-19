fda8fc6c6cfd3a00d1daa9931fbf1e201a969be6
tmh9x <thomas.hermanu@gmail.com>
2022-07-19 17:59:23 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Characters from "./views/Characters/Characters";
+import ButtonAppBar from "./components/ButtonAppBar/ButtonAppBar";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import CharacterDetails from "./views/CharacterDetails/CharacterDetails";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ButtonAppBar />
+      <Routes>
+        <Route path="/" element={<Characters />} />
+        <Route path=":id" element={<CharacterDetails />} />
+        <Route path="*" element={<h1>Route does not exist</h1>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
