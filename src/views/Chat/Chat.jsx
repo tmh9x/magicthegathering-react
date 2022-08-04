2190ccf306943d114564ba4d86a302adf209c97d
tmh9x <thomas.hermanu@gmail.com>
2022-08-04 16:32:07 +0200

    add delete and update function for chat

--- src/views/Chat/Chat.jsx
+++ src/views/Chat/Chat.jsx
@@ -1,9 +1,20 @@
 import "./Chat.css";
 
-import { Button, TextField } from "@mui/material";
+import { Button, IconButton, TextField } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
-import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  updateDoc,
+} from "firebase/firestore";
 
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { authContext } from "../../contexts/authContext";
 import { db } from "../../firebase";
 
@@ -20,11 +31,11 @@ export default function Chat() {
     return new Date(time * 1000).toLocaleString();
   };
 
-  const handleSendMsg = async () => {
+  const handleSendMsg = async (id) => {
     const newChatMsg = {
-      text: chatMsg,
       author: user.email,
       date: new Date(),
+      text: chatMsg,
     };
     try {
       const docRef = await addDoc(collection(db, "chat"), newChatMsg);
@@ -34,16 +45,35 @@ export default function Chat() {
     }
   };
 
+  const handleEditMsg = async (id) => {
+    console.log("id", id);
+
+    const text = prompt("Enter new text");
+
+    const docRef = doc(db, "chat", id);
+    const payload = { text };
+    updateDoc(docRef, payload);
+  };
+
+  const handleDeleteMsg = async (id) => {
+    const docRef = await deleteDoc(doc(db, "chat", id));
+    console.log("docRef", docRef.id);
+  };
+
   const getMessages = async () => {
     try {
-      const q = query(collection(db, "chat"));
+      const q = query(collection(db, "chat"), orderBy("date", "asc"));
       onSnapshot(q, (querySnapshot) => {
         const msgs = [];
         querySnapshot.forEach((doc) => {
-          msgs.push(doc.data());
+          const myMessage = {
+            id: doc.id,
+            data: doc.data(),
+          };
+          msgs.push(myMessage);
           console.log("msgs", msgs);
-          setMessages(msgs);
         });
+        setMessages(msgs);
       });
     } catch (error) {
       console.log("error :>> ", error);
@@ -54,23 +84,39 @@ export default function Chat() {
     getMessages();
   }, []);
 
+  console.log("messages", messages);
+
   return (
     <div className="chat-container">
       <h2>Welcome to our Chat</h2>
       {messages &&
-        messages.map((message, i) => {
-          return (
-            <div key={i} className="msg-container">
-              <div className="msg-container-header">
-                <p>{message.author}</p>
-                <p>{msgDate(message.date.seconds)}</p>
-              </div>
-              <div className="msg-container-body ">
-                <p>{message.text}</p>
+        messages.map((message, i) => (
+          <div key={i} className="msg-container">
+            <div className="msg-container-header">
+              <p>{message.data.author}</p>
+              <p>{msgDate(message.data.date.seconds)}</p>
+            </div>
+            <div className="msg-container-body ">
+              <p>{message.data.text}</p>
+              <div className="button-container">
+                <IconButton
+                  variant="contained"
+                  color="success"
+                  onClick={() => handleEditMsg(message.id)}
+                >
+                  <EditIcon />
+                </IconButton>
+                <IconButton
+                  variant="contained"
+                  color="error"
+                  onClick={() => handleDeleteMsg(message.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
 
       <div>
         <div className="msg-input">
